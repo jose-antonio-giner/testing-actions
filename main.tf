f7c1a6a15b3b249bae58474b6dc450da2a933547
Jose Antonio G <jogw@gft.com>
2022-03-08 18:53:07 +0000

    change

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-0011221"
   location = "europe-west1"
 }
-
-resource "google_storage_bucket" "gcs-bucket" {
-  name = "test-bucket-random-0011239"
-  location = "europe-west2"
-}
